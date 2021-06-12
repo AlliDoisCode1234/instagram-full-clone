56d20a14f779e569083cd5965051fed070c92b66
Josiah <josiah.james.fowler@gmail.com>
2021-06-12 18:40:35 -0400

    react app initialized

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+
+     {/* Header */}
+
+
+     {/* Posts */}
+     {/* Posts */}
+
+     
+    </div>
+  );
+}
+
+export default App;

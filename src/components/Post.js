3423fe36e16b1900528d96cb27e6c6d33ec47c23
Josiah <josiah.james.fowler@gmail.com>
2021-06-12 20:14:14 -0400

    post styles added

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import '../components/Post.css'
+import Avatar from "@material-ui/core/Avatar"
 
 const Post = () => {
     return (
         <div className="post">
-            <h3>Username</h3>
+            <div className="post__header">
+                <Avatar 
+                    className="post__avatar"
+                    alt="JosiahJames"
+                    src="/static/images/avatar/1/jpg"
+                />
+                <h3>Username</h3>
+            </div>
             {/* header => avatar + username */}
 
             <img src="https://www.petlandflorida.com/wp-content/uploads/2021/01/Petland_Florida_Toy_Poodle.jpg" alt="postImage" className="post__image" />
